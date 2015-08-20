8c067ef887cd68fa77eb39207968186fda7c7b6e
pploszaj91 <pploszaj91@gmail.com>
2015-08-20 18:17:02 +0200

    cos

--- dist/app/about/about.js
+++
@@ -1,6 +0,0 @@
-angular.module('about', ['dataSource']).
-
-controller('AboutCtrl', ['$scope', '$rootScope', 'dataSource',
-    function($scope, $rootScope, dataSource) {
-    }
-]);
--- dist/app/gallery/gallery.js
+++
@@ -1,6 +0,0 @@
-angular.module('gallery', ['dataSource']).
-
-controller('GalleryCtrl', ['$scope', '$rootScope', 'dataSource',
-    function($scope, $rootScope, dataSource) {
-    }
-]);
